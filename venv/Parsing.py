793e0ff9ca8459130243043ad0c7f08f53f3f4d6
Agaev Samir <Agaev_S@mail.ru>
2021-08-02 23:19:54 +0300

    pars local

--- venv/Parsing.py
+++ venv/Parsing.py
@@ -1,14 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://petrovich.ru/catalog/"
-headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
-}
-reg = requests.get(url, headers=headers)
-src=reg.text
-print(src)
-with open("index.html", "w", encoding='utf-8') as file:
-   file.write(src)
-
-# with open("index.html", "w", encoding='utf-8') as file:
+# url = "https://petrovich.ru/catalog/"
+# headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+# }
+# reg = requests.get(url, headers=headers)
+# src=reg.text
+# # print(src)
+# with open("index2.html", "w") as file:
 #    file.write(src)
+
+with open("index.html", encoding='utf-8' ) as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, "lxml")
+all_products_hrefs = soup.find_all(class_="main__catalog-title")
+for item in all_products_hrefs:
+   print(item.text)
